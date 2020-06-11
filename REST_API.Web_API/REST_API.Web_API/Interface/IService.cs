2ee0b77893ea115554f1d6aa81ac7c553b80a745
Almir98 <almir.tihak@edu.fit.ba>
2020-06-11 12:54:24 +0200

    .

---
+++ REST_API.Web_API/REST_API.Web_API/Interface/IService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_API.Interface
+{
+    public interface IService<TModel,Tsearch>
+    {
+        TModel Get(Tsearch search);
+        TModel GetById(int id);
+    }
+}
